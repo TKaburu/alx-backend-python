a928cdc5f16b220bb7ca2e06766d6076131a1222
TKaburu <traceymwendwa@gmail.com>
2024-05-02 23:37:11 +0300

    Feat: Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+""" to_str function """
+
+
+def to_str(n: float) -> str:
+    """
+    A  type-annotated function that returns
+    the string representation of the float
+    """
+    return str(n)
