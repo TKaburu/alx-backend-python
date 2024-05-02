16373a3cb20d3dd295b3cf81dd82f1b41c49e4c2
TKaburu <traceymwendwa@gmail.com>
2024-05-02 23:23:46 +0300

    Feat:  Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+""" concat function """
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    A  type-annotated function that returns a concatenated string
+    """
+    return str1 + str2
