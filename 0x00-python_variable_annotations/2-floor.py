0953a9812a9cdf79f236420a1fcb94195b863209
TKaburu <traceymwendwa@gmail.com>
2024-05-02 23:32:56 +0300

    Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+""" floor function"""
+
+
+import math
+
+
+def floor(n: float) -> float:
+    """
+    A  type-annotated function that returns the floor of a float
+    """
+    return math.floor(n)
